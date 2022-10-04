5be422c319a93886403a31263c68ae8879606b44
Juan Pablo Chavez Arteaga <juanp26842@gmail.com>
2022-10-03 23:23:10 -0400

    Admin table added to django admin page

--- schedules/admin.py
+++ schedules/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Schedule, User, Report
+from .models import AdminUser, Schedule, User, Report
 
 # Register your models here.
 
 admin.site.register(Schedule)
 admin.site.register(User)
 admin.site.register(Report)
+admin.site.register(AdminUser)
